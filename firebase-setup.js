aa3f63f08b041d996d6de0ec7143f9bff089fb24
S D the fucking G <140980492+megadeglitcher@users.noreply.github.com>
2024-11-10 01:33:49 +0200

    just testing

---
+++ firebase-setup.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCrxSA-Y5FjKCkULoQ3iwCiKaupZOSK9FU",
+  authDomain: "soniccdfansite.firebaseapp.com",
+  projectId: "soniccdfansite",
+  storageBucket: "soniccdfansite.firebasestorage.app",
+  messagingSenderId: "739250141699",
+  appId: "1:739250141699:web:1925788f3944b1aa58ac36",
+  measurementId: "G-EQK0WQWQ33"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
